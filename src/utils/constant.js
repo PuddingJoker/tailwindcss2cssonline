f00008c36ef89ed8eb56fab8c13efd8347210065
陈金鑫 <369790652@qq.com>
2023-07-20 15:14:12 +0800

    updaye

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -43,10 +43,40 @@ const specialReg = {
 
 const originClassHelper = ["hover", "sm", "md", "lg", "xl", "2xl"]
 
+const reactiveClass = [
+    [
+        "sm:",
+        "@media (min-width: 640px) { ... }",
+        ""
+    ],
+    [
+        "md:",
+        "@media (min-width: 768px) { ... }",
+        ""
+    ],
+    [
+        "lg:",
+        "@media (min-width: 1024px) { ... }",
+        ""
+    ],
+    [
+        "xl:",
+        "@media (min-width: 1280px) { ... }",
+        ""
+    ],
+    [
+        "2xl:",
+        "@media (min-width: 1536px) { ... }",
+        ""
+    ]
+]
+
+
 
 export {
     tailwindClass,
     specialClass,
     specialReg,
-    originClassHelper
+    originClassHelper,
+    reactiveClass
 }
